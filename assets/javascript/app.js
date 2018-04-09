007ff296a72148959e87a99b810a1506f7776fcc
Doug Kellermeyer <dxk558@case.edu>
2018-04-09 02:01:10 -0400

    animation and layout changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,16 @@
 $(document).ready(function(){
     $("#front-button").click(function(){
-        $("#landing-page").css("display", "none");
+        $("#landing-page").fadeOut(500);
     });
 });
+
+
+// background-image: url(https://images.pexels.com/photos/9754/mountains-clouds-forest-fog.jpg?auto=compress&cs=tinysrgb&h=1200&w=1920);
+// height: 100%;
+// width: 100%;
+// z-index: 1000;  
+// margin: auto;
+// position: absolute;
+// display: table;
+// background-size: cover;
+// }
