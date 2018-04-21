9e7e410b3a45f6affe89d484e0651f152bd28f79
Doug Kellermeyer <doug.kellermeyer@gmail.com>
2018-04-21 15:46:32 -0400

    moved landing page to a separate html file and added About Me html doc

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,8 +1,10 @@
-$(document).ready(function(){
-    $("#front-button").click(function(){
-        $("#landing-page").fadeOut(500);
-    });
-});
+//removed the jQuery fade out and opted for a landing page html 
+
+// $(document).ready(function(){
+//     $("#front-button").click(function(){
+//         $("#landing-page").fadeOut(500);
+//     });
+// });
 
 
 // background-image: url(https://images.pexels.com/photos/9754/mountains-clouds-forest-fog.jpg?auto=compress&cs=tinysrgb&h=1200&w=1920);
